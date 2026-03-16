e030294a81cc95786631ae69a32c7c9d8401753a
kyllian <kyllianazzrey2005@gmail.com>
2026-03-16 14:23:01 +0100

    first commit

---
+++ src/PanierTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+
+    @Test
+    void testSimple() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
